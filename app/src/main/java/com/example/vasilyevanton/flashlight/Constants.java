7662331b8de15a39997ff9a741e3e8434d339e89
Vasilyev Anton <vasilyevantonmihailovich@gmail.com>
2017-01-04 00:53:08 +0200

    anton_commit

---
+++ app/src/main/java/com/example/vasilyevanton/flashlight/Constants.java
@@ -0,0 +1,8 @@
+package com.example.vasilyevanton.flashlight;
+
+
+public class Constants {
+    public static final int PERMISSIONS_REQUEST_CAMERA = 100;
+    public static final int PERMISSIONS_REQUEST = 120;
+
+}
